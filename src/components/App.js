0e09b4b0fedd0c92126dfa32ebce3e9ec29dfdc2
DESKTOP-M398JT1\vbaud <vbaudin.dev@gmail.com>
2022-10-02 18:03:58 +0200

    Autopush

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+import Banner from './Banner.js'
+
+const App = () => {
+  return (
+    <Banner />
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import '../styles/Banner.css';
+
+const Banner = () => {
+    return (
+        <div>Ma banniere</div>
+    )
+}
+
+export default Banner;
